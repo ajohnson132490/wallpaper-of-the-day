14e85a603344e0a1d3871646b8d48220b1db38b4
ajohnson132490 <44075033+ajohnson132490@users.noreply.github.com>
2021-10-24 15:39:09 -0400

    Continued learning

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# knowledge-for-everyone
-Web scraper designed to get rid of paywalls on news articles or information articles
+# wallpaper-of-the-day
+Web scraper designed to find a cool HD wallpaper and set it as your desktop background
 
 ### Tutorial I followed for the learning branch
 https://www.youtube.com/watch?v=Xjv1sY630Uc&list=PLzMcBGfZo4-n40rB1XaJ0ak1bemvlqumQ
+
+###Ideas for other Selenium projects
+https://www.quora.com/What-are-some-of-the-Selenium-Python-projects-for-Beginners
--- webScrapper.py
+++ webScrapper.py
@@ -1,12 +1,16 @@
 import os
+import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 PATH = os.getcwd() + r"\Web-Drivers\chromedriver.exe"
 s = Service(PATH)
-
 driver = webdriver.Chrome(service = s)
+wait = WebDriverWait(driver, 10)
 
 #Opens a webpage
 driver.get("https://techwithtim.net")
@@ -14,5 +18,22 @@ driver.get("https://techwithtim.net")
 #Prints the page title
 print(driver.title)
 
-#Closes a webpage
-driver.quit()
+#Locate the search bar and search for test
+search = wait.until(EC.presence_of_element_located((By.NAME, "s")))
+search.send_keys("test")
+search.send_keys(Keys.RETURN)
+
+#Waiting until the page is loaded up
+try:
+    main = wait.until(
+        EC.presence_of_element_located((By.ID, "main"))
+    )
+
+    articles = main.find_elements_by_tag_name("article")
+
+    for article in articles:
+        header = article.find_element_by_class_name("entry-summary")
+        print(header.text)
+
+finally:
+    driver.quit()
