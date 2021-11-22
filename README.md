795523e3d746efc7288b38696b69da753f4a3554
ajohnson132490 <44075033+ajohnson132490@users.noreply.github.com>
2021-11-22 00:52:20 -0500

    Downloads everytime, even with ads. Also removes the file when done.

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # wallpaper-of-the-day
-This tool gets a new, HD wallpaper for you everyday and automatically sets it as your background
-
-Still has issues with ads.
+This tool gets a new, HD wallpaper for you everyday and automatically sets it as your background.
--- webScraper.py
+++ webScraper.py
@@ -10,10 +10,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
+#Setting download folder
 chrome_options = webdriver.ChromeOptions()
 prefs = {'download.default_directory' : os.getcwd() + r"\Resources"}
 chrome_options.add_experimental_option('prefs', prefs)
 chrome_options.add_argument("--disable-notifications")
+
 
 # Setting important variables
 PATH = os.getcwd() + r"\Resources\chromedriver.exe"
@@ -26,24 +28,14 @@ downloadName = ""
 # Opens the webpage
 driver.get("https://bing.gifposter.com/list/new/desc/classic.html?p=1")
 
-# Prints the page title
-print(driver.title)
-
 # Waiting until the page is loaded up
 try:
     mainList = wait.until(
         EC.presence_of_element_located((By.TAG_NAME, "ul"))
     )
 
-    print("Main list has been loaded.")
-
     # Getting all the wallpapers into a list
     wallpaperOptions = mainList.find_elements_by_tag_name("li")
-
-    # Cycling through all of the wallpapers and printing the name of the wallpaper
-    for wallpaper in wallpaperOptions:
-        name = wallpaper.find_element_by_tag_name("span")
-        print(name.text)
 
     # Randomly selecting a wallpaper
     wallpaper = wallpaperOptions[rand.randrange(len(wallpaperOptions))]
@@ -58,16 +50,11 @@ except Exception as e:
 # Open the wallpaper page
 driver.get(wallpaperPage)
 
-# Prints the page title
-print(driver.title)
-
 # Waiting until the page is loaded up
 try:
     mainDiv = wait.until(
         EC.presence_of_element_located((By.XPATH, "//div[@class='clearfix']"))
     )
-
-    print("Wallpaper page has been loaded.")
 
     # Getting the link to the full screen wallpaper page
     fullScreenView = mainDiv.find_element_by_tag_name("a").get_attribute("href")
@@ -78,36 +65,25 @@ except Exception as e:
 # Open the Full Screen View
 driver.get(fullScreenView)
 
-# Prints the page title
-print(driver.title)
-
 # Waiting until the page is loaded up
 try:
     cont = True
     while(cont):
+        cont = False
         mainDiv = wait.until(
             EC.presence_of_element_located((By.XPATH, "//div[@class='nav-btn']"))
             )
 
-        print("Full Screen page has been loaded.")
-
         # Downloading the wallpaper
         downloadName = mainDiv.find_element(By.XPATH, "//a[contains (@class, 'icon download')]").get_attribute("download")
         mainDiv.find_element(By.XPATH, "//a[contains (@class, 'icon download')]").click()
-
         time.sleep(3)
-
         numFiles = 0
         for path in os.listdir(os.getcwd() + "\\Resources"):
             numFiles += 1
-
-        if numFiles > 1:
-            cont = False
-
-        driver.navigate().refresh()
-
-    print("Download Complete")
-
+        if numFiles == 1:
+            cont = True
+            driver.refresh()
 except Exception as e:
     print(f"{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}")
 
@@ -116,5 +92,10 @@ finally:
     driver.quit()
 
 # Set the wallpaper as desktop background
-ctypes.windll.user32.SystemParametersInfoW(20,0, os.getcwd() + "\\Resources\\{0}".format(downloadName),0)
+WALLPAPER_PATH = os.getcwd() + "\Resources\\" + downloadName
+ctypes.windll.user32.SystemParametersInfoW(20, 0, WALLPAPER_PATH , 3)
+
+# Remove wallpaper from folder
+os.remove(WALLPAPER_PATH)
+
 print("Program Complete")
