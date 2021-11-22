a9a501c1b41a9ba30665a311d94b2345b82bdb00
ajohnson132490 <44075033+ajohnson132490@users.noreply.github.com>
2021-11-22 01:17:07 -0500

    Adds the exe and resources to startup folder

---
+++ dist/install.bat
@@ -0,0 +1,3 @@
+@echo off
+move webScraper.exe "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
+move Resources "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
