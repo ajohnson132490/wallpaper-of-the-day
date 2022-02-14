2991e88123442e373d742eb436df9e4d9875a26d
Austin <ajohnson132490@gmail.com>
2022-02-14 02:28:36 -0500

    Updated installer for single file

--- dist/install.bat
+++ dist/install.bat
@@ -1,4 +1,2 @@
 @echo off
-mkdir "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Resources"
-copy Resources "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Resources\"
 copy webScraper.exe "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
